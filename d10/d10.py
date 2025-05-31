28acee7cb1b9d175bcbf7287289ace72447eab95
ykghani <yusuf@chicagobooth.edu>
2025-05-31 19:11:58 -0400

    Part 2 solved

--- d10/d10.py
+++ d10/d10.py
@@ -1,5 +1,6 @@
 '''Advent of Code 2020 Day 10 - Adapter Array'''
 from pathlib import Path
+from collections import defaultdict
 
 script_dir = Path(__file__).parent
 file_path = script_dir / 'd10.txt'
@@ -24,3 +25,12 @@ for idx, adapter in enumerate(adapters):
 
 print(jolt_differential)
 print(f"Part 1: {jolt_differential[1] * jolt_differential[3]}")
+
+adapters.insert(0, 0)
+ways = defaultdict(int)
+ways[0] = 1
+
+for adapter in adapters[1: ]:
+    ways[adapter] = (ways[adapter - 1] + ways[adapter - 2] + ways[adapter - 3])
+
+print(f"Part two: {ways[adapters[-1]]}")
